ca0e5af73ace32067282197fed52c325882f3a61
rahima-b <BBXRAH001@myuct.ac.za>
2025-02-24 08:18:11 +0200

    Create install.R
    I have added the packages I installed

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse") 
+install.packages("readr") 
+install.packages("readxl") 
+install.packages("dplyr") 
+install.packages("tidyr") 
+install.packages("ggplot2") 
+install.packages("lubridate")
